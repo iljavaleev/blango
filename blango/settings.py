91d0a1be66fb8b19fda8bba06c8ee484a8ec15d5
Валеев Равилевич <iljavaleev@gmail.com>
2023-01-19 08:18:26 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -184,7 +184,8 @@ class Dev(Configuration):
             "level": "DEBUG",
         },
     }
-
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    ACCOUNT_ACTIVATION_DAYS = 7
 
 class Prod(Dev):
     DEBUG = values.BooleanValue(True)
